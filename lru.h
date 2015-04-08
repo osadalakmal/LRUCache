2fb0818eb846e7a316309af8e9277ef635dbbe4c
Osada Paranaliyanage <osadalakmal@gmail.com>
2015-04-08 08:45:04 +0100

    Added locking to add thread safety to the LRU cache

--- lru.h
+++ lru.h
@@ -5,6 +5,7 @@
 #include <list>
 #include <memory>
 #include <cstddef>
+#include <mutex>
 
 template<typename KEY, typename VALUE>
 class LruCache {
@@ -14,6 +15,14 @@ class LruCache {
   LruCache& operator=(const LruCache&);
   LruCache&& operator=(const LruCache&&);
   public:
+    // Typedefs Publicly Available
+    typedef std::list<KEY> KEY_LIST_TYPE;
+    typedef typename KEY_LIST_TYPE::iterator KEY_LIST_ITERATOR;
+    typedef typename std::pair<VALUE, KEY_LIST_ITERATOR> DATA_MAP_ELEMENT;
+    typedef typename std::map<KEY, DATA_MAP_ELEMENT> DATA_MAP_TYPE;
+    typedef typename DATA_MAP_TYPE::iterator DATA_MAP_ITERATOR;
+   
+    // Public Interface
     LruCache(const size_t cacheSize);
     ~LruCache();
     void add(const KEY& key, const VALUE& value);
@@ -21,20 +30,19 @@ class LruCache {
   private:
     void evict(const size_t numOfElements);
 
-    typedef std::list<KEY> KEY_LIST_TYPE;
-    typedef typename KEY_LIST_TYPE::iterator KEY_LIST_ITERATOR;
-    typedef typename std::pair<VALUE, KEY_LIST_ITERATOR> DATA_MAP_ELEMENT;
-    typedef typename std::map<KEY, DATA_MAP_ELEMENT> DATA_MAP_TYPE;
-    typedef typename DATA_MAP_TYPE::iterator DATA_MAP_ITERATOR;
     const int d_cacheSize;
     DATA_MAP_TYPE d_dataMap;
     KEY_LIST_TYPE d_timeOrderedList;
+    std::mutex d_accessMutex;
+
 };
 
 #endif
 
 template <typename KEY, typename VALUE>
-LruCache<KEY,VALUE>::LruCache(const size_t cacheSize) : d_cacheSize(cacheSize) {
+LruCache<KEY,VALUE>::LruCache(const size_t cacheSize) : d_cacheSize(cacheSize),
+  d_accessMutex()
+{
 }
 
 template <typename KEY, typename VALUE>
@@ -45,6 +53,7 @@ LruCache<KEY,VALUE>::~LruCache() {
 
 template <typename KEY, typename VALUE>
 void LruCache<KEY,VALUE>::add(const KEY& key, const VALUE& value) {
+  std::lock_guard<std::mutex> accessLockGuard(d_accessMutex);
   if (d_dataMap.size() == d_cacheSize) {
     evict(1);
   }
@@ -58,6 +67,7 @@ template <typename KEY, typename VALUE>
 std::unique_ptr<VALUE> LruCache<KEY,VALUE>::get(const KEY& key) {
   DATA_MAP_ITERATOR iter = d_dataMap.find(key);
   if ( iter != d_dataMap.end() ) {
+    std::lock_guard<std::mutex> accessLockGuard(d_accessMutex);
     d_timeOrderedList.splice(d_timeOrderedList.begin(), d_timeOrderedList, iter->second.second);
     return std::unique_ptr<VALUE>( new VALUE(iter->second.first) );;
   } else {
